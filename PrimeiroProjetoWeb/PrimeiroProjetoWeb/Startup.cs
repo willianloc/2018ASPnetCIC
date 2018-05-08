441732118a5e57cb00c2d1ae1744bd242df9cddc
Willian Douglas Gobbi <1527621@educacional.up>
2018-05-08 20:17:43 -0300

    First commit

---
+++ PrimeiroProjetoWeb/PrimeiroProjetoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrimeiroProjetoWeb.Startup))]
+namespace PrimeiroProjetoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
