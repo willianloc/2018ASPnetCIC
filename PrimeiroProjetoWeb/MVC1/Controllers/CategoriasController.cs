8ab3908c40865f82dbf534467837cde644a7a919
Willian Douglas Gobbi <1527621@educacional.up>
2018-05-15 20:47:11 -0300

    MVC1

---
+++ PrimeiroProjetoWeb/MVC1/Controllers/CategoriasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<string> categorias = new List<string>();
+
+            categorias.Add("Estudo");
+            categorias.Add("Lesma");
+            categorias.Add("Jão");
+            categorias.Add("Trabalho");
+
+            ViewBag.MinhasCategorias = categorias;
+
+            return View();
+        }
+    }
+}
